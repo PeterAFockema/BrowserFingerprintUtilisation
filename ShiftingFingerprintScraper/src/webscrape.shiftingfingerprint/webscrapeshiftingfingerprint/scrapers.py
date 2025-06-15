b225ea3f6392db049e0f5ce0c4db9072afecde0e
PeterAFockema <anonymous@person.com>
2025-06-15 22:46:49 +0100

    Added chrome and firefox driver implementations for a passed url

--- ShiftingFingerprintScraper/src/webscrape.shiftingfingerprint/webscrapeshiftingfingerprint/scrapers.py
+++ ShiftingFingerprintScraper/src/webscrape.shiftingfingerprint/webscrapeshiftingfingerprint/scrapers.py
@@ -36,3 +36,31 @@ class Scrapers(object):
         print(driver.title)
 
         #Here we close the browser when done
+        driver.quit()
+
+
+    def chrome_driver_implementation_passed_url(self, passed_url):
+        # Initialize the Chrome driver
+        driver = webdriver.Chrome()
+
+        # Navigate to the URL
+        driver.get(passed_url)
+
+        # Print the title page
+        print(driver.title)
+
+        # Here we close the browser when done
+        driver.quit()
+
+    def firefox_driver_implementation_passed_url(self, passed_url):
+        #Initialise the Firefox driver
+        driver = webdriver.Firefox()
+
+        #Navigate to the URL
+        driver.get(passed_url)
+
+        #Print the title page
+        print(driver.title)
+
+        #Here we close the browser when done
+        driver.quit()
