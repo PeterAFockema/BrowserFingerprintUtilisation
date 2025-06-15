c2ff62501bd58e60136fd55d5e1b100ff0aa4659
PeterAFockema <anonymous@person.com>
2025-06-15 22:54:15 +0100

    Used chrome and firefox driver implementations for passed url of our basic website

--- run_scraper_code.py
+++ run_scraper_code.py
@@ -4,7 +4,16 @@ from webscrapeshiftingfingerprint.scrapers import Scrapers
 
 if __name__=="__main__":
     scrapers = Scrapers()
-    print("Chrome driver implementation...")
-    scrapers.chrome_driver_implementation()
-    print("Firefox driver implementation...")
-    scrapers.firefox_driver_implementation()
+
+    file = open('URL_values.txt')
+    for line in file:
+        fields = line.strip().split()
+        if fields[0]=="URL":
+            print("URL is: ", fields[1])
+            scrapers.chrome_driver_implementation_passed_url(fields[1])
+            scrapers.firefox_driver_implementation_passed_url(fields[1])
+
+    #print("Chrome driver implementation...")
+    #scrapers.chrome_driver_implementation()
+    #print("Firefox driver implementation...")
+    #scrapers.firefox_driver_implementation()
