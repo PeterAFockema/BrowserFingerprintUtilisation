d8487af0ff5c790eee50dda428c0673607b62dc7
PeterAFockema <anonymous@person.com>
2025-06-18 22:53:50 +0100

    Updated the package with an set of defs which take Options as an argument.

--- ShiftingFingerprintScraper/src/webscrape.shiftingfingerprint/webscrapeshiftingfingerprint/scrapers.py
+++ ShiftingFingerprintScraper/src/webscrape.shiftingfingerprint/webscrapeshiftingfingerprint/scrapers.py
@@ -1,7 +1,8 @@
 #scrapers.py
 
 from selenium import webdriver
-#from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options as OptionsChrome
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
 #from selenium.webdriver.firefox.service import Service
 
 class Scrapers(object):
@@ -15,26 +16,20 @@ class Scrapers(object):
     def chrome_driver_implementation(self):
         # Initialize the Chrome driver
         driver = webdriver.Chrome()
-
         # Navigate to the URL
         driver.get('https://google.com')
-
         # Print the title page
         print(driver.title)
-
         # Here we close the browser when done
         driver.quit()
 
     def firefox_driver_implementation(self):
         #Initialise the Firefox driver
         driver = webdriver.Firefox()
-
         #Navigate to the URL
         driver.get('https://google.com')
-
         #Print the title page
         print(driver.title)
-
         #Here we close the browser when done
         driver.quit()
 
@@ -42,25 +37,51 @@ class Scrapers(object):
     def chrome_driver_implementation_passed_url(self, passed_url):
         # Initialize the Chrome driver
         driver = webdriver.Chrome()
-
         # Navigate to the URL
         driver.get(passed_url)
-
         # Print the title page
         print(driver.title)
-
         # Here we close the browser when done
         driver.quit()
 
     def firefox_driver_implementation_passed_url(self, passed_url):
         #Initialise the Firefox driver
         driver = webdriver.Firefox()
-
         #Navigate to the URL
         driver.get(passed_url)
-
         #Print the title page
         print(driver.title)
+        #Here we close the browser when done
+        driver.quit()
 
+    def chrome_options(self):
+        options = OptionsChrome()
+        options.headless = True #Enable headless mode
+        options.add_argument("--window-size=1920, 1920") #Defined window browser size
+        return options 
+
+    def chrome_driver_implementation_passed_url_and_options(self, passed_url, options):
+        # Initialize the Chrome driver
+        driver = webdriver.Chrome(options=options)
+        # Navigate to the URL
+        driver.get(passed_url)
+        # Print the title page
+        print(driver.title)
+        # Here we close the browser when done
+        driver.quit()
+
+    def firefox_options(self):
+        options = OptionsFirefox()
+        options.headless = True #Enable headless mode
+        options.add_argument("--window-size=1920, 1920") #Defined window browser size
+        return options 
+
+    def firefox_driver_implementation_passed_url_and_options(self, passed_url, options):
+        #Initialise the Firefox driver
+        driver = webdriver.Firefox(options=options)
+        #Navigate to the URL
+        driver.get(passed_url)
+        #Print the title page
+        print(driver.title)
         #Here we close the browser when done
         driver.quit()
