0e69c69df03379bc02a7ab14ba4df85fc4cdff82
PeterAFockema <anonymous@person.com>
2025-07-20 15:04:35 +0100

    Added a test package to use to test the code that will be used in the canvas extension.

---
+++ ScrapeHTML/test_manager.py
@@ -0,0 +1,9 @@
+from ScrapeHTML.pull_web_html_chrome import *
+
+class TestManager():
+    'Test Manager class used for testing software'
+    
+    html_puller_chrome = HTMLPullerUsingChrome()
+
+    def __init__(self) -> None:
+        pass
