ed1f46bcfa369902ac657d74762625e4e3fe8943
PeterAFockema <anonymous@person.com>
2026-01-11 13:32:12 +0000

    Added options for firefox to be later completed for the canvas.

--- ScrapeHTML/test_manager.py
+++ ScrapeHTML/test_manager.py
@@ -1,9 +1,11 @@
 from ScrapeHTML.pull_web_html_chrome import *
+from ScrapeHTML.pull_web_html_firefox import *
 
 class TestManager():
     'Test Manager class used for testing software'
     
     html_puller_chrome = HTMLPullerUsingChrome()
+    html_puller_firefox = HTMLPullerUsingFirefox()
 
     def __init__(self) -> None:
         pass
--- features/steps/testing_canvas.py
+++ features/steps/testing_canvas.py
@@ -1,5 +1,3 @@
-import requests
-import configparser
 from behave import *
 from bs4 import BeautifulSoup
 from ScrapeHTML.test_manager import *
@@ -45,30 +43,30 @@ The following definitions relate to the Firefox browser.
 '''
 @given('we declare a Firefox server defined for canvas values with no extension')
 def we_declare_a_firefox_server_defined_for_canvas_values_with_no_extension(context):
-    assert test_manager.html_puller_chrome.check_can_pull_HTML_page != False
+    assert test_manager.html_puller_firefox.check_can_pull_HTML_page != False
 
 @when('we view the Firefox page with no canvas values interference')
 def we_view_the_firefox_page_with_no_canvas_values_interference(context):
-    html_puller_chrome = test_manager.html_puller_chrome
-    bool(BeautifulSoup(html_puller_chrome.pull_HTML_page(), "html.parser").find())
-    test_manager.html_puller_chrome= html_puller_chrome
-    assert test_manager.html_puller_chrome.html_source != "<html></html>"
+    html_puller_firefox = test_manager.html_puller_firefox
+    bool(BeautifulSoup(html_puller_firefox.pull_HTML_page(), "html.parser").find())
+    test_manager.html_puller_firefox= html_puller_firefox
+    assert test_manager.html_puller_firefox.html_source != "<html></html>"
 
 @then('we have a Firefox page which ran the canvas response')
 def we_have_a_firefox_page_which_ran_the_canvas_response(context):
-    assert test_manager.html_puller_chrome.html_source != "<html></html>"
+    assert test_manager.html_puller_firefox.html_source != "<html></html>"
 
 @then('the Firefox canvas value has been recorded')
 def the_firefox_canvas_value_has_been_recorded(context):
-    assert test_manager.html_puller_chrome.get_canvas_value() != None
+    assert test_manager.html_puller_firefox.get_canvas_value() != None
 
 @given('we declare a Firefox server defined for canvas values with an extension')
 def we_declare_a_firefox_server_defined_for_canvas_values_with_an_extension(context):
-    assert test_manager.html_puller_chrome.check_can_pull_HTML_page != False
+    assert test_manager.html_puller_firefox.check_can_pull_HTML_page != False
 
 @when('we view the Firefox page with some canvas values interference')
 def we_view_the_firefox_page_with_some_canvas_values_interference(context):
-    html_puller_chrome = test_manager.html_puller_chrome
-    bool(BeautifulSoup(html_puller_chrome.pull_HTML_page_with_canvas_extension(), "html.parser").find())
-    test_manager.html_puller_chrome= html_puller_chrome
+    html_puller_firefox = test_manager.html_puller_firefox
+    bool(BeautifulSoup(html_puller_firefox.pull_HTML_page_with_canvas_extension(), "html.parser").find())
+    test_manager.html_puller_chrome= html_puller_firefox
     assert test_manager.html_puller_chrome.html_source != "<html></html>" 
