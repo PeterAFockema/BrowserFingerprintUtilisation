26e9501ae9966f6a7178463af75be92a5ec0c4bf
PeterAFockema <anonymous@person.com>
2026-03-31 07:37:07 +0100

    Updating test cases usage for fonts, webgl, screen, and canvas.

---
+++ features/steps/testing_font.py
@@ -0,0 +1,39 @@
+from behave import *
+from bs4 import BeautifulSoup
+from ScrapeHTML.test_manager import *
+
+test_manager = TestManager()
+font_increment = 0
+
+'''
+The following definitions relate to the Firefox browser.
+'''
+@given('we declare a Firefox server defined for font values with no extension')
+def we_declare_a_firefox_server_defined_for_font_values_with_no_extension(context):
+    assert test_manager.html_puller_firefox.check_can_pull_HTML_page != False
+
+@when('we view the Firefox page with no font values interference')
+def we_view_the_firefox_page_with_no_font_values_interference(context):
+    html_puller_firefox = test_manager.html_puller_firefox
+    bool(BeautifulSoup(html_puller_firefox.pull_HTML_page(), "html.parser").find())
+    test_manager.html_puller_firefox= html_puller_firefox
+    assert test_manager.html_puller_firefox.html_source != "<html></html>"
+
+@then('we have a Firefox page which ran the font response')
+def we_have_a_firefox_page_which_ran_the_font_response(context):
+    assert test_manager.html_puller_firefox.html_source != "<html></html>"
+
+@then('the Firefox font value has been recorded')
+def the_firefox_font_value_has_been_recorded(context):
+    assert test_manager.html_puller_firefox.get_canvas_value() != None
+
+@given('we declare a Firefox server defined for font values with an extension')
+def we_declare_a_firefox_server_defined_for_font_values_with_an_extension(context):
+    assert test_manager.html_puller_firefox.check_can_pull_HTML_page != False
+
+@when('we view the Firefox page with some font values interference')
+def we_view_the_firefox_page_with_some_font_values_interference(context):
+    html_puller_firefox = test_manager.html_puller_firefox
+    bool(BeautifulSoup(html_puller_firefox.pull_HTML_page_with_extension("font"), "html.parser").find())
+    test_manager.html_puller_chrome= html_puller_firefox
+    assert test_manager.html_puller_chrome.html_source != "<html></html>" 
---
+++ features/testing_webgl.feature
@@ -0,0 +1,12 @@
+Feature: Checking webgl element fingerprinting responses
+    Scenario: Preliminary Firefox test results for no extension to change the webgl value
+        Given we declare a Firefox server defined for webgl values with no extension
+        When we view the Firefox page with no webgl values interference
+        Then we have a Firefox page which ran the webgl response
+        Then the Firefox webgl value has been recorded
+    
+    Scenario: Preliminary Firefox test results for an extension to change the webgl value
+        Given we declare a Firefox server defined for webgl values with an extension
+        When we view the Firefox page with some webgl values interference
+        Then we have a Firefox page which ran the webgl response
+        Then the Firefox webgl value has been recorded
