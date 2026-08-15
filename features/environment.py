7398d1a3b2a6a4f9de30bc5caa48adb9a9edaf20
PeterAFockema <anonymous@person.com>
2026-08-15 14:08:28 +0100

    Updated the README.md.

--- features/environment.py
+++ features/environment.py
@@ -1,15 +1,19 @@
 from ScrapeHTML.test_manager import *
 from ScrapeHTML.defined_values import *
 
+from shiftingbrowserfingerprints.generate_xpis import build_all_unsigned_xpi_extensions
+
+
 """ 
 The code within the following block is checked before all/any of test steps are run.
 This would be a great place to instantiate any of your class objects and store them as
 attributes in behave's context object for later use.
 """
 def before_scenario(self, context):         
-    # The following creates an api_calls attribute for behave's context object
-    # context.api_calls = ApiClient(context.config.userdata['url'])
-    # test_manager.html_puller_firefox
-    print("In environment.py initialising TestManager...")
+    print("In environment.py. Initialising TestManager...")
     test_manager = TestManager()
     print("After initialising TestManager...")
+
+def before_feature(context, feature):
+    print(f"In environment.py. We Are Starting Feature: {feature.name}")
+    build_all_unsigned_xpi_extensions()
--- run_scraper_code.py
+++ run_scraper_code.py
@@ -1,36 +1,40 @@
 #run_scraper_code.py
 
-from shiftingbrowserfingerprints.scrapers_objects import Scrapers
+from shiftingbrowserfingerprints.firefox_scrapers import FirefoxScrapers as Scrapers
+from shiftingbrowserfingerprints.generate_xpis import build_all_unsigned_xpi_extensions
 
 if __name__=="__main__":
-    scrapers = Scrapers()
 
-    file = open('URL_values.txt')
-    for line in file:
-        fields = line.strip().split()
-        if fields[0]=="URL":
-            # print("URL is: ", fields[1])
-            # scrapers.chrome_driver_implementation_passed_url(fields[1])
-            # scrapers.firefox_driver_implementation_passed_url(fields[1])
-            # print("Now with options given...")
-            # options= scrapers.firefox_options()
-            # scrapers.firefox_driver_implementation_passed_url_and_options(fields[1], options)
-            # options= scrapers.chrome_options()
-            # scrapers.chrome_driver_implementation_passed_url_and_options(fields[1], options)
-            # print("Now Chrome with mobile parameters...")
-            # scrapers.chrome_driver_implementation_passed_url_mobile(fields[1])
-            # print("Now Firefox with mobile parameters...")
-            # scrapers.firefox_driver_implementation_passed_url_mobile(fields[1])
-            # print("Now Chrome with tablet parameters...")
-            # scrapers.chrome_driver_implementation_passed_url_tablet(fields[1])
-            # print("Now Firefox with tablet parameters...")
-            # scrapers.firefox_driver_implementation_passed_url_tablet(fields[1])
-            # print("Now Chrome with desktop parameters...")
-            # scrapers.chrome_driver_implementation_passed_url_desktop(fields[1])
-            # print("Now Firefox with desktop parameters...")
-            # scrapers.firefox_driver_implementation_passed_url_desktop(fields[1])
-            # print("Now Chrome with Hello World extension...")
-            # scrapers.chrome_driver_extension_implementation()
-            print("Now Chrome with Firefox extension...")
-            # scrapers.chrome_driver_canvas_extension_implementation()
-            scrapers.firefox_driver_extension_string_implementation("font")
+    build_all_unsigned_xpi_extensions()
+
+    # scrapers = Scrapers()
+
+    # file = open('URL_values.txt')
+    # for line in file:
+    #     fields = line.strip().split()
+    #     if fields[0]=="URL":
+    #         print("URL is: ", fields[1])
+    #         # scrapers.chrome_driver_implementation_passed_url(fields[1])
+    #         # scrapers.firefox_driver_implementation_passed_url(fields[1])
+    #         # print("Now with options given...")
+    #         # options= scrapers.firefox_options()
+    #         # scrapers.firefox_driver_implementation_passed_url_and_options(fields[1], options)
+    #         # options= scrapers.chrome_options()
+    #         # scrapers.chrome_driver_implementation_passed_url_and_options(fields[1], options)
+    #         # print("Now Chrome with mobile parameters...")
+    #         # scrapers.chrome_driver_implementation_passed_url_mobile(fields[1])
+    #         # print("Now Firefox with mobile parameters...")
+    #         # scrapers.firefox_driver_implementation_passed_url_mobile(fields[1])
+    #         # print("Now Chrome with tablet parameters...")
+    #         # scrapers.chrome_driver_implementation_passed_url_tablet(fields[1])
+    #         # print("Now Firefox with tablet parameters...")
+    #         # scrapers.firefox_driver_implementation_passed_url_tablet(fields[1])
+    #         # print("Now Chrome with desktop parameters...")
+    #         # scrapers.chrome_driver_implementation_passed_url_desktop(fields[1])
+    #         # print("Now Firefox with desktop parameters...")
+    #         # scrapers.firefox_driver_implementation_passed_url_desktop(fields[1])
+    #         # print("Now Chrome with Hello World extension...")
+    #         # scrapers.chrome_driver_extension_implementation()
+    #         print("Now with Firefox extension...")
+    #         # scrapers.chrome_driver_canvas_extension_implementation()
+    #         scrapers.firefox_driver_extension_string_implementation("font")
